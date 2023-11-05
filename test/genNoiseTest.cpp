27c0f5d3d77fb77bb0bb4c194665bd3cf52b0284
AnatolyBalabinsky <anatoly.balabinsky@gmail.com>
2023-11-05 22:23:34 +0300

    add tests for generators

--- test/genNoiseTest.cpp
+++ test/genNoiseTest.cpp
@@ -1,12 +1,35 @@
 #include <gtest/gtest.h>
-#include "../include/DSP/fft.h"
+#include "../../DSP/include/DSP/fft.h"
+#include "../include/receiver/gen_noise.h"
+#include "../../Base/include/Base/writer.h"
 #include <vector>
 #include <fstream>
 
-TEST( genNoiseTest, firstTest ) {
+using namespace Base;
 
-    int i = 1;
-    int j = 1;
-    ASSERT_EQ(i,j);
+TEST( TestNoise, WNFine ) {
+	Fftw fftw;
+	GenNoise GenN;
+	float W = 10;
+	uint64_t N = 4096 * 16;
 
+	std::vector< Complex< float > > data_ = GenN.GenWN< float >( W, N );
+
+    fftw.Forward< float >( data_.data(), data_.data(), data_.size() );
+
+	float avr = 0;
+	for( uint64_t i = 0; i < N; i++ ) {
+		avr += ( data_[ i ].abs() * data_[ i ].abs() );
+	}
+    avr /= (N * N);
+    ASSERT_NEAR( avr, pow( 10, W / 10 ), pow( 10, W / 10 ) * 1e-2 );
+
+	Writer write;
+	std::vector< float > vec( 2 * N );
+	for( uint64_t i = 0; i < N; i++ ) {
+        vec[ 2 * i ] = data_[ i ].re;
+        vec[ 2 * i + 1 ] = data_[ i ].im;
+	}
+
+	write.writeFile( "/home/maksim/work/w_ml/testNoise", vec, 1 );
 }
--- test/genSinTest.cpp
+++ test/genSinTest.cpp
@@ -1,12 +1,69 @@
 #include <gtest/gtest.h>
-#include "../include/DSP/fft.h"
 #include <vector>
+#include "../../Base/include/Base/complex.h"
+#include "../include/receiver/gen_sin.h"
+#include <cmath>
 #include <fstream>
 
-TEST( genSinTest, firstTest ) {
+TEST( genSinTest, output_values ) {
 
-    int i = 1;
-    int j = 1;
-    ASSERT_EQ(i,j);
+    std::ifstream file( "dat/test_val_sin", std::ifstream::binary );
+    if( !file.is_open() ) {
+        throw std::runtime_error( "cant open file " );
+    }
+
+    std::vector< Complex< float > > vec( 1000 );
+
+    for( uint32_t i = 0; i < 1000; i++ ) {
+        file.read( reinterpret_cast< char* >( &( vec[ i ].re ) ), sizeof( float ) );
+        file.read( reinterpret_cast< char* >( &( vec[ i ].im ) ), sizeof( float ) );
+    }
+    file.close();
+
+    Generator_sin test_obj;
+
+    std::vector< Complex< float > > data = test_obj.gen_sin< float >( 1, 10, 1600, 1000 );
+
+    for( uint64_t i = 0; i < 1000; ++i ) {
+
+        // if( data[ i ].re() != vec[ i ].re() ) std::cout << i << std::endl;
+
+        ASSERT_NEAR( data[ i ].re, vec[ i ].re, 1e-10 );
+        ASSERT_NEAR( data[ i ].im, vec[ i ].im, 1e-10 );
+
+    }
+}
+
+TEST( sin_test, null_size ) {
+
+    Generator_sin test_obj;
+
+    try {
+
+        std::vector< Complex< float > > data = test_obj.gen_sin< float >( 1, 10, 1600, 0 );
+
+
+    } catch( std::runtime_error& e ) {
+
+        ASSERT_STREQ( e.what(), "length cant be equal 0" ) << "something wrong\n";
+
+    }
+
+}
+
+TEST( sin_test, null_fd ) {
+
+    Generator_sin test_obj;
+
+    try {
+
+        std::vector< Complex< float > > data = test_obj.gen_sin< float >( 1, 10, 0, 100 );
+
+
+    } catch( std::runtime_error& e ) {
+
+        ASSERT_STREQ( e.what(), "Fd cant be equal 0" ) << "something wrong\n";
+
+    }
 
 }
