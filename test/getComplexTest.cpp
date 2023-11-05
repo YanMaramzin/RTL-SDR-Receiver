b4ead5d0e6b37773390e5e014fee75457cbec74c
AnatolyBalabinsky <anatoly.balabinsky@gmail.com>
2023-11-05 18:35:56 +0300

    add true tests for checking

---
+++ test/getComplexTest.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+#include "../include/DSP/fft.h"
+#include <vector>
+#include <fstream>
+
+TEST( getComplexTest, firstTest ) {
+
+    int i = 1;
+    int j = 1;
+    ASSERT_EQ(i,j);
+
+}
---
+++ test/getSpectrumTest.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+#include "../include/DSP/fft.h"
+#include <vector>
+#include <fstream>
+
+TEST( getSpectrumTest, firstTest ) {
+
+    int i = 1;
+    int j = 1;
+    ASSERT_EQ(i,j);
+
+}
